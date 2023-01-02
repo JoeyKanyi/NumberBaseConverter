8f21ff2b72c7a6e03d1fee8dff4041926565a2a9
JoeyKanyi <joeymkanyi@gmail.com>
2023-01-03 00:04:41 +0300

    Change Allow more than Decimal conversion

--- Binary-to-decimal.cpp
+++ Binary-to-decimal.cpp
@@ -3,22 +3,35 @@
 #include <unistd.h>
 using namespace std;
 
+/*
+     Does not include decimal conversion functions
+*/
 int main()
 {
+     cout << "Only supports binary conversion without decimal(10^-) conversion functions" << endl;
      while (true)
      {
+          // Initialize the variables
           string in;
           int out = 0;
+          int b = 0;
+          char Q;
+
+          // Inputs the number for a conversion as a string
           cout << "Enter Binary Number : ";
           cin >> in;
-          int n = in.size();
 
+          // Create a char array of the same size as the input string
+          int n = in.size();
           char x[n];
+
+          // Converts the input string to a char array
           for (int i = 0; i < n; i++)
           {
                x[i] = in[i];
           }
-          int b = 0;
+
+          // Does the actual conversion from a binary number to the array
           for (int i = n - 1; i >= 0; i--)
           {
 
@@ -33,8 +46,11 @@ int main()
                }
                b++;
           }
+
+          // Gives the output
           cout << "Decimal Equivalent: " << out << endl;
-          char Q;
+
+          // Queries the user whether to close the program
           cout << "Do you want to continue? (y/n): " << endl;
           cin >> Q;
           if (Q == 'y' || Q == 'Y')
@@ -43,7 +59,7 @@ int main()
           }
           else if (Q == 'n' || Q == 'N')
           {
-               cout << "Thank you" << endl;
+               cout << "Thank you. Goodbye" << endl;
                break;
           }
      }
--- Main.cpp
+++ Main.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
 #include <cmath>
 #include <unistd.h>
+#include <string>
 using namespace std;
+
+// Initialise Global Variables
+int out = 0;
+int b = 0;
+char Q;
+string in;
+string inputbase;
+int n = in.size();
+
+// Make a binary Converter function
+int BinarytoDecimal(char input)
+{
+     
+     for (int i = n - 1; i >= 0; i--)
+     {
+
+          if (input[i] == '0')
+          {
+               out = out + 0;
+          }
+          else if (input[i] == '1')
+          {
+               double y{std::pow(2, b)};
+               out = out + y;
+          }
+          b++;
+     }
+     return out;
+}
 
 int main()
 {
      while (true)
      {
-          string in;
-          int out = 0;
-          cout << "Enter Binary Number : ";
-          cin >> in;
-          int n = in.size();
+          // Initialize the variables
+          string inputNumber;
+          int inputbase;
+          int outputbase;
+
+          // Input the inputbase and number to be converted
+          cout << "Enter input base : ";
+          cin >> inputbase;
+
+          cout << "Enter Target Base : ";
+          cin >> outputbase;
+
+          cout << "Enter Number : ";
+          cin >> inputNumber;
+
+          // Call the conversion function
+          int n = inputNumber.size();
           char x[n];
+          // Converts the input string to a char array
           for (int i = 0; i < n; i++)
           {
-               x[i] = in[i];
+               x[i] = inputNumber[i];
           }
-          int b = 0;
-          for (int i = n - 1; i >= 0; i--)
-          {
+          return x;
 
-               if (x[i] == '0')
-               {
-                    out = out + 0;
-               }
-               else if (x[i] == '1')
-               {
-                    double y{std::pow(2, b)};
-                    out = out + y;
-               }
-               b++;
+          // Call Functions
+          if (inputbase == 2 && outputbase == 10){
+               out = BinarytoDecimal(x);
+               cout << "Decimal Equivalent: " << out << endl; // Outputs the converted number
+          }else{
+               cout << "Invalid inputbase " << inputbase << endl
+                    << "Please try again." << endl;
           }
-          cout << "Decimal Equivalent: " << out << endl;
-          char Q;
+          
+          // Queries the user to continue
           cout << "Do you want to continue? (y/n): " << endl;
           cin >> Q;
           if (Q == 'y' || Q == 'Y')
